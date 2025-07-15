fc09ea5f8d9749650a5f797cbeed30657e3e53bb
a1icia-m <alicia.m@berkeley.edu>
2025-07-15 00:50:06 -0700

    Updated dashboard page and initialized database

--- components/header.jsx
+++ components/header.jsx
@@ -1,56 +1,59 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { Button } from './ui/button';
-import { LayoutDashboard, PenBox } from 'lucide-react';
+import React from "react";
+import { Button } from "./ui/button";
+import { PenBox, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
 
-const Header = () => {
-  return (
-    <div className = "fixed top-0 w-full bg-white/80 backdrop-blue-md z-50 border-b" >
-        <nav className= "container mx-auto px-4 py-4 flex items-center justify-between">
-            <Link href= '/'>
-                <Image src ={"/logo.png"}
-                alt = 'welth logo'
-                height ={60}
-                width = {200}
-                className = "h-12 w auto object-contain"
-                />
-            </Link>
+const Header = async() => {
+    await checkUser();
 
-            <div className = "flex items-center space-x-4">
-                <SignedIn>
-                    <Link href={"/dasbhoard"} 
-                    className = "text-gray-600 hover:text-blue-600 flex items-center gap-2">
-                        <Button variant ="outline">
-                            <LayoutDashboard size={18}/>
-                            <span className = "hidden md:inline">Dashboard</span>
-                        </Button>
-                    </Link>
+    return (
+        <div className = "fixed top-0 w-full bg-white/80 backdrop-blue-md z-50 border-b" >
+            <nav className= "container mx-auto px-4 py-4 flex items-center justify-between">
+                <Link href= '/'>
+                    <Image src ={"/logo.png"}
+                    alt = 'welth logo'
+                    height ={60}
+                    width = {200}
+                    className = "h-12 w auto object-contain"
+                    />
+                </Link>
 
-                    <Link href={"/transaction/create"}>
-                        <Button className = "flex items-center gap-2">
-                            <PenBox size={18}/>
-                            <span className = "hidden md:inline">Add Transaction</span>
-                        </Button>
-                    </Link>
-                </SignedIn>
+                <div className = "flex items-center space-x-4">
+                    <SignedIn>
+                        <Link href={"/dashboard"} 
+                        className = "text-gray-600 hover:text-blue-600 flex items-center gap-2">
+                            <Button variant ="outline">
+                                <LayoutDashboard size={18}/>
+                                <span className = "hidden md:inline">Dashboard</span>
+                            </Button>
+                        </Link>
 
-                <SignedOut>
-                    <SignInButton forceRedirectUrl='/dashboard'>
-                        <Button variant ="outline">Login</Button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton appearnce={{
-                        elemnts: {
-                            avatarBox: "w-20 h-20",
-                        },
-                    }}/>
-                </SignedIn>
-            </div>
-      </nav>
-    </div>
+                        <Link href={"/transaction/create"}>
+                            <Button className = "flex items-center gap-2">
+                                <PenBox size={18}/>
+                                <span className = "hidden md:inline">Add Transaction</span>
+                            </Button>
+                        </Link>
+                    </SignedIn>
+
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl='/dashboard'>
+                            <Button variant ="outline">Login</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton appearnce={{
+                            elemnts: {
+                                avatarBox: "w-20 h-20",
+                            },
+                        }}/>
+                    </SignedIn>
+                </div>
+        </nav>
+        </div>
   );
 };
 
